91e5fa803302aada1914da9870f5c39eda3d6331
luotj <luotj@ganguo.hk>
2021-02-03 18:09:59 +0800

    feat:新增zuul网关整合swagger

--- tangel-springcloud-consumer80/src/main/java/com/tangel/springcloud/ConsumerApplication80.java
+++ tangel-springcloud-consumer80/src/main/java/com/tangel/springcloud/ConsumerApplication80.java
@@ -3,10 +3,12 @@ package com.tangel.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * 消费者80启动类
  */
+@EnableSwagger2
 @EnableEurekaServer
 @SpringBootApplication
 public class ConsumerApplication80 {
